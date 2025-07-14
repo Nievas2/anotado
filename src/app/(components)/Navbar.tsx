7b8bc0e8be4a2a6249e8a49f7ec9afea51b52b71
Nievas2 <angelgabrielnievas@gmail.com>
2025-07-14 15:29:27 -0300

    style(Navbar): :bug: Change sticky to static

--- src/app/(components)/Navbar.tsx
+++ src/app/(components)/Navbar.tsx
@@ -5,7 +5,7 @@ import IconTheme from "./IconTheme"
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between w-full sticky top-0 px-4 p-2 max-w-8xl bg-slate-600">
+    <nav className="flex justify-between w-full px-4 p-2 max-w-8xl bg-slate-600">
       <Link href="/" className="text-2xl font-bold p-2">
         <button className="rounded-lg flex items-center justify-center ">
           <span className="text-white font-bold text-sm">Anotado</span>
