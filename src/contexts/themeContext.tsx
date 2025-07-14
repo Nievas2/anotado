6e4569df7e6e84646b14f6e5a0b0255c805c0e68
Nievas2 <angelgabrielnievas@gmail.com>
2025-07-14 15:16:52 -0300

    fix(General): :bug: Fix types themeProvider

--- src/contexts/themeContext.tsx
+++ src/contexts/themeContext.tsx
@@ -1,14 +1,11 @@
 "use client"
 
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({
   children,
   ...props
-}: {
-  children: React.ReactNode,
-  [key: string]: any
-}) {
+}: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
