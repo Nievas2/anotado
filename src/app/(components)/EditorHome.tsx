0b8c5f56073bdc9a645ce146f08132dbb0524caf
Nievas2 <angelgabrielnievas@gmail.com>
2025-07-16 13:57:23 -0300

    fix(Home): :bug: Add dynamic import editor

--- src/app/(components)/EditorHome.tsx
+++ src/app/(components)/EditorHome.tsx
@@ -1,51 +1,14 @@
 "use client"
-import { useCreateBlockNote } from "@blocknote/react"
-import { BlockNoteView } from "@blocknote/mantine"
-import "@blocknote/core/fonts/inter.css"
-import "@blocknote/mantine/style.css"
+import dynamic from "next/dynamic"
+import React, { useMemo } from "react"
 
 export default function EditorHome() {
-  const editor = useCreateBlockNote({
-    initialContent: [
-      {
-        type: "heading",
-        content: "Anotado",
-      },
-      {
-        type: "paragraph",
-        content:
-          "Anotado es una sencilla app para tomar notas, desarrollada con BlockNote.",
-      },
-      {
-        type: "paragraph",
-        content: "Explora las funciones y personaliza tus notas.",
-      },
-      {
-        type: "paragraph",
-        content: "¡Que disfrutes tomando notas!",
-      },
-      {
-        type: "paragraph",
-        content: "",
-      },
-    ],
-  })
-  // Guarda el contenido anterior para poder volver si se pasa de 8
-  let previousContent = editor.document
-
-  editor.onChange(() => {
-    const length = editor.document.length
-
-    if (length > 8) {
-      // Revertimos al estado anterior si se excede el límite
-      editor.replaceBlocks(editor.document, previousContent)
-    } else {
-      // Guardamos el contenido válido actual
-      previousContent = editor.document
-    }
-  })
-
-  return (
-    <BlockNoteView title="Anotado" editor={editor} className="p-4 w-full" />
+  const EditorHomeClient = useMemo(
+    () =>
+      dynamic(() => import("./EditorHomeClient"), {
+        ssr: false,
+      }),
+    []
   )
+  return <EditorHomeClient />
 }
