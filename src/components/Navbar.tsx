c20ec1551e5401bdf9211bfae18db0262e6c09eb
Nievas2 <angelgabrielnievas@gmail.com>
2025-07-18 12:57:15 -0300

    feat(Editor): :sparkles: add param id

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ import IconTheme from "./IconTheme"
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between w-full px-4 p-2 max-w-8xl bg-slate-600 h-16">
-      <Link href="/" className="text-2xl font-bold p-2 w-full">
+    <nav className="flex items-center justify-between w-full px-4 p-2 max-w-8xl bg-slate-600 h-16">
+      <Link href="/" className="text-2xl font-bold p-2">
         <button className="rounded-lg flex items-center justify-center ">
           <span className="text-white font-bold text-sm">Anotado</span>
         </button>
--- src/components/editors/Editor.tsx
+++ src/components/editors/Editor.tsx
@@ -1,15 +1,16 @@
 "use client" // this registers <Editor> as a Client Component
 import { useCreateBlockNote } from "@blocknote/react"
-import { BlockNoteView } from "@blocknote/mantine";
-import "@blocknote/core/fonts/inter.css";
-import "@blocknote/mantine/style.css";
+import { BlockNoteView } from "@blocknote/mantine"
+import "@blocknote/core/fonts/inter.css"
+import "@blocknote/mantine/style.css"
 
 // Our <Editor> component we can reuse later
-export default function Editor() {
+export default function Editor({ id }: { id: string }) {
   // Creates a new editor instance.
   const editor = useCreateBlockNote()
-  
+
   // Renders the editor instance using a React component.
-  return <BlockNoteView title="Anotado" editor={editor} className="p-4 w-full" />
-  
+  return (
+    <BlockNoteView title="Anotado" editor={editor} className="p-4 w-full" />
+  )
 }
